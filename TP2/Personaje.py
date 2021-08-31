8e757fcd270a6edc7e39da6d82030bfd483c2a48
Pedro <pelopez@itba.edu.ar>
2021-08-31 18:00:47 -0300

    Los Personajes tienen guardados los IDs del equipamiento

--- TP2/Personaje.py
+++ TP2/Personaje.py
@@ -1,26 +1,24 @@
 import math
-import random
 from dataclasses import dataclass, field
 
-from armamentos.Armamento import Armamento
-from armamentos.Arma import Arma
-
+from DataReader import DataReader
 from clases.Clase import Clase
 
 
-@dataclass(frozen=True)
 class Personaje:
-    clase: Clase = field()
-    altura: float = field()
-    bota: Armamento = field()
-    casco: Armamento = field()
-    guante: Armamento = field()
-    pechera: Armamento = field()
-    arma: Armamento = field()
+    clase: Clase
+    altura: float
+    equipment: list[int]
+    # 0 -> weapon
+    # 1 -> boots
+    # 2 -> helmet
+    # 3 -> gloves
+    # 4 -> chest plate
 
-    @property
-    def clase(self):
-        return self.clase
+    def __init__(self, clase: Clase, height: float, weapon_id: int, boots_id: int, helmet_id: int, gloves_id: int, chestplate_id: int):
+        self.clase = clase
+        self.altura = height
+        self.equipment = [weapon_id, boots_id, helmet_id, gloves_id, chestplate_id]
 
     def atm(self):
         base = 3 * self.altura - 5
@@ -37,16 +35,66 @@ class Personaje:
         return (self.resistencia() + self.pericia()) * self.vida() * self.dem()
 
     def fuerza(self):
-        return 100 * math.tanh(0.01 * (self.bota.fuerza + self.casco.fuerza + self.guante.fuerza + self.pechera.fuerza + self.arma.fuerza))
+        return 100 * math.tanh(0.01 * self._sum_fuerza())
 
     def agilidad(self):
-        return math.tanh(0.01 * (self.bota.agilidad + self.casco.agilidad + self.guante.agilidad + self.pechera.agilidad + self.arma.agilidad))
+        return math.tanh(0.01 * self._sum_agilidad())
 
     def pericia(self):
-        return 0.6 * math.tanh(0.01 * (self.bota.pericia + self.casco.pericia + self.guante.pericia + self.pechera.pericia + self.arma.pericia))
+        return 0.6 * math.tanh(0.01 * self._sum_pericia())
 
     def resistencia(self):
-        return math.tanh(0.01 * (self.bota.resistencia + self.casco.resistencia + self.guante.resistencia + self.pechera.resistencia + self.arma.resistencia))
+        return math.tanh(0.01 * self._sum_resistencia())
 
     def vida(self):
-        return 100 * math.tanh(0.01 * (self.bota.vida + self.casco.vida + self.guante.vida + self.pechera.vida + self.arma.vida))
+        return 100 * math.tanh(0.01 * self._sum_vida())
+
+    def _sum_vida(self):
+        if DataReader.reader is None:
+            raise Exception('DataReader must be initialized')
+        boots = DataReader.reader.get('boots', self.equipment[1]).vida
+        helmet = DataReader.reader.get('helmet', self.equipment[2]).vida
+        gloves = DataReader.reader.get('gloves', self.equipment[3]).vida
+        chestplate = DataReader.reader.get('chestplate', self.equipment[4]).vida
+        weapon = DataReader.reader.get('weapon', self.equipment[0]).vida
+        return boots + helmet + gloves + chestplate + weapon
+
+    def _sum_resistencia(self):
+        if DataReader.reader is None:
+            raise Exception('DataReader must be initialized')
+        boots = DataReader.reader.get('boots', self.equipment[1]).resistencia
+        helmet = DataReader.reader.get('helmet', self.equipment[2]).resistencia
+        gloves = DataReader.reader.get('gloves', self.equipment[3]).resistencia
+        chestplate = DataReader.reader.get('chestplate', self.equipment[4]).resistencia
+        weapon = DataReader.reader.get('weapon', self.equipment[0]).resistencia
+        return boots + helmet + gloves + chestplate + weapon
+
+    def _sum_pericia(self):
+        if DataReader.reader is None:
+            raise Exception('DataReader must be initialized')
+        boots = DataReader.reader.get('boots', self.equipment[1]).pericia
+        helmet = DataReader.reader.get('helmet', self.equipment[2]).pericia
+        gloves = DataReader.reader.get('gloves', self.equipment[3]).pericia
+        chestplate = DataReader.reader.get('chestplate', self.equipment[4]).pericia
+        weapon = DataReader.reader.get('weapon', self.equipment[0]).pericia
+        return boots + helmet + gloves + chestplate + weapon
+
+    def _sum_agilidad(self):
+        if DataReader.reader is None:
+            raise Exception('DataReader must be initialized')
+        boots = DataReader.reader.get('boots', self.equipment[1]).agilidad
+        helmet = DataReader.reader.get('helmet', self.equipment[2]).agilidad
+        gloves = DataReader.reader.get('gloves', self.equipment[3]).agilidad
+        chestplate = DataReader.reader.get('chestplate', self.equipment[4]).agilidad
+        weapon = DataReader.reader.get('weapon', self.equipment[0]).agilidad
+        return boots + helmet + gloves + chestplate + weapon
+
+    def _sum_fuerza(self):
+        if DataReader.reader is None:
+            raise Exception('DataReader must be initialized')
+        boots = DataReader.reader.get('boots', self.equipment[1]).fuerza
+        helmet = DataReader.reader.get('helmet', self.equipment[2]).fuerza
+        gloves = DataReader.reader.get('gloves', self.equipment[3]).fuerza
+        chestplate = DataReader.reader.get('chestplate', self.equipment[4]).fuerza
+        weapon = DataReader.reader.get('weapon', self.equipment[0]).fuerza
+        return boots + helmet + gloves + chestplate + weapon
